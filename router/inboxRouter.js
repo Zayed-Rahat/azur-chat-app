2495d6964abebe59b6e2f6a6d3e1ef9deafa1d56
Zayed-Rahat <azurahat129@gmail.com>
2022-04-25 23:11:01 +0600

    back-to-lesson-3

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -4,11 +4,10 @@ const express = require("express");
 // internal imports
 const { getInbox } = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-const { checkLogin } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
 // inbox page
-router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+router.get("/", decorateHtmlResponse("Inbox"), getInbox);
 
 module.exports = router;
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -15,17 +15,14 @@ const {
   addUserValidationHandler,
 } = require("../middlewares/users/userValidators");
 
-const { checkLogin } = require("../middlewares/common/checkLogin");
-
 const router = express.Router();
 
 // users page
-router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
+router.get("/", decorateHtmlResponse("Users"), getUsers);
 
 // add user
 router.post(
   "/",
-  checkLogin,
   avatarUpload,
   addUserValidators,
   addUserValidationHandler,
